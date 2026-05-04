9c0dc40685acce18b24054203daaf9a27c65583b
Hongsinwon <ghdtlsdnjs12@naver.com>
2026-05-04 20:43:58 +0900

    feat : react-router-dom 페이지 이동 및 SPA 라우터 이해

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Login from './pages/Login.jsx';
+import Home from './pages/Home.jsx';
+
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
+      <BrowserRouter>
+            <nav>
+                <Link to="/">홈</Link> | <Link to="/login">로그인</Link>
+            </nav>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
